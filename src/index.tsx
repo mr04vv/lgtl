b5ecb705390b45c753735e4723d0a20009121d50
Takuto Mori <mr04vv@gmail.com>
2024-01-13 16:35:04 +0900

    :sparkles: feat: fix api

--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,11 @@ import { Home } from "./Home";
 const app = new Hono();
 
 app.get("*", renderer);
-
-const a =
-  "https://media.gettyimages.com/id/155284552/ja/%E3%82%B9%E3%83%88%E3%83%83%E3%82%AF%E3%83%95%E3%82%A9%E3%83%88/tabby-%E7%8C%AB%E3%81%AE%E3%83%9D%E3%83%BC%E3%83%88%E3%83%AC%E3%83%BC%E3%83%88%E7%99%BD%E8%83%8C%E6%99%AF.jpg?s=612x612&w=gi&k=20&c=oHieCgMLvy2CnOmxxoUmbMrxd0uumMUrbHELZX2eO_g=";
-const b =
-  "https://img.freepik.com/free-photo/closeup-of-a-cute-cat-sitting-on-the-carpet-against-a-blurred-background_181624-53498.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705017600&semt=sph";
-
 interface Env {
   R2_BUCKET: R2Bucket;
   R2_URL: string;
   LATTE_TOKEN: string;
 }
-
-const shuffleArray = (array: string[]) => {
-  return array.slice().sort(() => Math.random() - Math.random());
-};
 
 app.get("/", async (c) => {
   const obj = await (c.env as unknown as Env).R2_BUCKET.list();
@@ -28,7 +18,7 @@ app.get("/", async (c) => {
     return new Response("Not found", { status: 404 });
   }
   const imageUrls = obj.objects.map((o) => `${r2Url}/${o.key}`);
-  return c.render(<Home imageUrls={shuffleArray(imageUrls)} />);
+  return c.render(<Home imageUrls={imageUrls} />);
 });
 
 app.get("/api", async (c) => {
@@ -43,7 +33,7 @@ app.get("/api", async (c) => {
   const imageUrls = obj.objects.map((o) => `${r2Url}/${o.key}`);
 
   return c.json({
-    imageUrls: shuffleArray(imageUrls),
+    imageUrls,
   });
 });
 
